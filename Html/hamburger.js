7bec036f9c9660cd38b1ad0b11fd07245e164e9c
Jason1806 <89740354+Jason1806@users.noreply.github.com>
2023-09-10 17:39:11 +0530

    Delete Html directory

--- Html/hamburger.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.querySelector(".menu");
-    const navLinks = document.querySelector(".nav-container");
-
-    menuToggle.addEventListener("click", function () {
-        navLinks.classList.toggle("active");
-    });
-});
